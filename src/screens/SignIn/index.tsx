ea7935bba89567bee6b024f8b53a1f74cb3e6a87
leokazuyukinagatani <lknlkn15@hotmail.com>
2023-08-15 21:04:25 -0300

    feat: login with google and configs

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { Alert } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+
+import backgroundImg from "../../assets/background.png";
+
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from "@env";
+import { Container, Slogan, Title } from "./styles";
+import { Button } from "../../components/Button";
+
+WebBrowser.maybeCompleteAuthSession();
+
+export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const [_, response, googleSignIn] = Google.useAuthRequest({
+    androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    scopes: ["profile", "email"],
+  });
+
+  async function handleGoogleSignIn() {
+    setIsAuthenticating(true);
+    googleSignIn().then((response) => {
+      if (response.type !== "success") {
+        setIsAuthenticating(false);
+      }
+    });
+  }
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      if (response.authentication?.idToken) {
+        fetch(
+          `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${response.authentication.idToken}`
+        )
+          .then((response) => response.json())
+          .then(console.log);
+      } else {
+        Alert.alert(
+          "Entrar",
+          "Não foi possível conectar-se a sua conta Google."
+        );
+        setIsAuthenticating(false);
+      }
+    }
+  }, [response]);
+
+  return (
+    <Container source={backgroundImg}>
+      <Title>Ignite Fleet</Title>
+
+      <Slogan>Gestão de uso de veículos</Slogan>
+      <Button
+        title="Entrar com Goggle"
+        isLoading={isAuthenticating}
+        onPress={() => handleGoogleSignIn()}
+      />
+    </Container>
+  );
+}
