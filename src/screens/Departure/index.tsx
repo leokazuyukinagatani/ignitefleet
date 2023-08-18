6e33ee8f4530d2ddb02c1cb1e2e58fe10bbce618
leokazuyukinagatani <lknlkn15@hotmail.com>
2023-08-17 22:50:41 -0300

    feat: LicensePlateInput

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import { Container } from './styles';
-import { Header } from '../../components/Header';
+import React from "react";
+import { Container, Content } from "./styles";
+import { Header } from "../../components/Header";
+import { LicensePlateInput } from "../../components/LicensePlateInput";
 
 export function Departure() {
   return (
     <Container>
-      <Header title='Saída' />
+      <Header title="Saída" />
+      <Content>
+        <LicensePlateInput label="Placa do veiculo" placeholder="BRA1234" />
+      </Content>
     </Container>
   );
 }
--- src/screens/Departure/styles.ts
+++ src/screens/Departure/styles.ts
@@ -1,6 +1,13 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  background-color: ${({theme})=> theme.COLORS.GRAY_800};
+  background-color: ${({ theme }) => theme.COLORS.GRAY_800};
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  gap: 10px;
+  padding: 32px;
+  margin-top: 16px;
 `;
