67d83f57c088b8ac3a5efab71eb3b99ac7ef028b
leokazuyukinagatani <lknlkn15@hotmail.com>
2023-08-20 20:41:16 -0300

    feat: scham coords

--- src/libs/realm/schemas/Historic.ts
+++ src/libs/realm/schemas/Historic.ts
@@ -1,10 +1,12 @@
 import { Realm } from "@realm/react";
-
+import { CoordsSchemaProps } from "./Coords";
 type GenerateProps = {
   user_id: string;
   license_plate: string;
   description: string;
+  coords: CoordsSchemaProps[];
 };
+
 export class Historic extends Realm.Object<Historic> {
   _id!: string;
   user_id!: string;
@@ -13,19 +15,26 @@ export class Historic extends Realm.Object<Historic> {
   status!: string;
   created_at!: Date;
   updated_at!: Date;
+  coords!: CoordsSchemaProps[];
 
-  static generate({ description, license_plate, user_id }: GenerateProps) {
+  static generate({
+    description,
+    license_plate,
+    user_id,
+    coords,
+  }: GenerateProps) {
     return {
       _id: new Realm.BSON.UUID(),
       user_id,
       license_plate,
       description,
+      coords,
       status: "departure",
       created_at: new Date(),
       updated_at: new Date(),
     };
   }
-  
+
   static schema = {
     name: "Historic",
     primaryKey: "_id",
@@ -34,6 +43,10 @@ export class Historic extends Realm.Object<Historic> {
       user_id: {
         type: "string",
         indexedDB: true,
+      },
+      coords: {
+        type:'list',
+        objectType: 'Coords'
       },
       license_plate: "string",
       description: "string",
