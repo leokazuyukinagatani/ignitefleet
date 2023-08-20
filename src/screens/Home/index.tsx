72b7e0a6b4615196ea535df99d65a52ec2ec31c0
leokazuyukinagatani <lknlkn15@hotmail.com>
2023-08-19 23:39:51 -0300

    feat: historic in home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,14 +3,19 @@ import React, { useEffect, useState } from "react";
 import { useQuery, useRealm } from "../../libs/realm";
 import { Historic } from "../../libs/realm/schemas/Historic";
 
-import { Container, Content } from "./styles";
+import { Container, Content, Label, Title } from "./styles";
 import { HomeHeader } from "../../components/HomeHeader";
 import { useNavigation } from "@react-navigation/native";
 import { CarStatus } from "../../components/CarStatus";
-import { Alert } from "react-native";
+import { Alert, FlatList } from "react-native";
+import { HistoricCard, HistoricCardProps } from "../../components/HistoricCard";
+import dayjs from "dayjs";
 
 export function Home() {
   const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null);
+  const [vehicleHistoric, setVehicleHistoric] = useState<HistoricCardProps[]>(
+    []
+  );
   const { navigate } = useNavigation();
   const realm = useRealm();
 
@@ -37,10 +42,38 @@ export function Home() {
     }
   }
 
+  function fetchHistoric() {
+    try {
+      const response = historic.filtered(
+        "status = 'arrival' SORT(created_at DESC)"
+      );
+
+      const formattedHistoric = response.map((historic) => {
+        return {
+          id: historic._id.toString(),
+          licensePlate: historic.license_plate,
+          isSync: false,
+          created: dayjs(historic.created_at).format(
+            "[Saída em] DD/MM/YYYY [às] HH:mm"
+          ),
+        };
+      });
+
+      setVehicleHistoric(formattedHistoric);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Histórico", "Não foi possível carregar o histórico.");
+    }
+  }
+
   useEffect(() => {
     fetchVehicleInUse();
+  }, []);
+
+  useEffect(() => {
+    fetchHistoric();
   }, [historic]);
-  
+
   useEffect(() => {
     realm.addListener("change", () => fetchVehicleInUse());
 
@@ -54,6 +87,17 @@ export function Home() {
         <CarStatus
           licensePlate={vehicleInUse?.license_plate}
           onPress={handleRegisterMovement}
+        />
+        <Title>Historico</Title>
+        <FlatList
+          data={vehicleHistoric}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <HistoricCard data={item} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 100 }}
+          ListEmptyComponent={() => (
+            <Label>Nenhum registro de utilização</Label>
+          )}
         />
       </Content>
     </Container>
